b3eeca26cc2e626fd6c17a849c4a9db191f3e870
matheuspoleza <matheus.poleza@contaazul.com>
2016-04-15 15:57:19 -0300

    returning name service

--- {{cookiecutter.app_name}}/src/client/app/+home/components/home.component.ts
+++ {{cookiecutter.app_name}}/src/client/app/+home/components/home.component.ts
@@ -1,6 +1,8 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
 
+import {NameListService} from '../../shared/index';
+
 @Component({
   selector: 'sd-home',
   templateUrl: 'app/+home/components/home.component.html',
@@ -8,5 +10,16 @@ import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/common';
   directives: [FORM_DIRECTIVES, CORE_DIRECTIVES]
 })
 export class HomeComponent {
-  constructor() {}
+  newName: string;
+  constructor(public nameListService: NameListService) {}
+
+  /*
+   * @param newname  any text as input.
+   * @returns return false to prevent default form submit behavior to refresh the page.
+   */
+  addName(): boolean {
+    this.nameListService.add(this.newName);
+    this.newName = '';
+    return false;
+  }
 }
